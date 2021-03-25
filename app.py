f374faa300bf6483bc308d81a852fe73921dd526
mayanknariani <44632739+mayanknariani@users.noreply.github.com>
2021-03-26 04:36:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 import FundamentalAnalysis as fa
 import nltk
+import yfinance as yf
+yf.pdr_override()
+from sklearn import preprocessing
+from keras.models import Model
+from keras.layers import Dense, Dropout, LSTM, Input, Activation, concatenate
+from keras import optimizers
+import numpy as np
+import math
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+np.random.seed(4)
+import tensorflow
+tensorflow.random.set_seed(4)
 import streamlit as st
 import matplotlib.pyplot as plt, pandas as pd, numpy as np
 import matplotlib
@@ -10,9 +22,6 @@ from fbprophet import Prophet
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-from datetime import datetime
-from functools import reduce
-from Tech_ind import *
 from keras.layers.recurrent import GRU
 from alpha_vantage.foreignexchange import ForeignExchange
 from matplotlib.pyplot import rc
@@ -32,8 +41,6 @@ from keras.layers import LSTM
 import tweepy
 import pandas as pd
 from textblob import TextBlob
-import preprocessor as p
-#from preprocessor import api
 import re
 
 
@@ -53,6 +60,130 @@ def loadData(ticker, start, end):
      df_stockdata = pdr.get_data_yahoo(ticker, start= str(start), end = str(end))
      df_stockdata.index = pd.to_datetime(df_stockdata.index)
      return df_stockdata
+
+
+def lstm_model_tech(lstm_input,dense_input,ohlcv_train,tech_ind_train,y_train):
+        # the first branch operates on the first input
+        x = LSTM(25, name='lstm_0')(lstm_input)
+        x = Dropout(0.1, name='lstm_dropout_0')(x)
+        lstm_branch = Model(inputs=lstm_input, outputs=x)
+
+        # the second branch opreates on the second input
+        y = Dense(25, name='tech_dense_0')(dense_input)
+        y = Activation("relu", name='tech_relu_0')(y)
+        y = Dropout(0.1, name='tech_dropout_0')(y)
+        technical_indicators_branch = Model(inputs=dense_input, outputs=y)
+
+        # combine the output of the two branches
+        combined = concatenate([lstm_branch.output, technical_indicators_branch.output], name='concatenate')
+
+        z = Dense(64, activation="sigmoid", name='dense_pooling')(combined)
+        z = Dense(1, activation="linear", name='dense_out')(z)
+
+        # our model will accept the inputs of the two branches and
+        # then output a single value
+        model = Model(inputs=[lstm_branch.input, technical_indicators_branch.input], outputs=z)
+        adam = optimizers.Adam(lr=0.0005)
+        model.compile(loss='mean_squared_error',optimizer=adam)
+        model.fit(x=[ohlcv_train, tech_ind_train], y=y_train, batch_size=10, epochs=20, shuffle=True, validation_split=0.3)
+
+        return model
+
+def tech_ind():
+    data = loadData(ticker,start,end)
+    #data = data.drop('Datetime', axis=1)
+    data = data.drop('Volume', axis=1)
+    data = data.drop('Adj Close', axis=1)
+    data = data.drop('High', axis=1)
+    data = data.drop('Open', axis=1)
+    data = data.drop('Low', axis=1)
+    data = data.values
+
+    data_normaliser = preprocessing.MinMaxScaler()
+    data_normalised = data_normaliser.fit_transform(data)
+
+    history_points = 50
+    import numpy as np
+    # using the last {history_points} open close high low volume data points, predict the next open value
+    ohlcv_histories_normalised = np.array([data_normalised[i:i + history_points].copy() for i in range(len(data_normalised) - history_points)])
+
+
+    next_day_open_values_normalised = np.array([data_normalised[:, 0][i + history_points].copy() for i in range(len(data_normalised) - history_points)])
+
+
+    next_day_open_values_normalised = np.expand_dims(next_day_open_values_normalised, -1)
+    next_day_open_values = np.array([data[:, 0][i + history_points].copy() for i in range(len(data) - history_points)])
+    next_day_open_values = np.expand_dims(next_day_open_values, -1)
+    y_normaliser = preprocessing.MinMaxScaler()
+
+    y_normaliser.fit(next_day_open_values)
+
+    technical_indicators = []
+    def calc_ema(values, time_period):
+        # https://www.investopedia.com/ask/answers/122314/what-exponential-moving-average-ema-formula-and-how-ema-calculated.asp
+        sma = np.mean(values[:, 0])
+        ema_values = [sma]
+        k = 2 / (1 + time_period)
+        for i in range(len(his) - time_period, len(his)):
+            close = his[i][0]
+            ema_values.append(close * k + ema_values[-1] * (1 - k))
+        return ema_values[-1]
+
+    for his in ohlcv_histories_normalised:
+        #Taking the SMA of the closing price to calcualte the MACF
+        sma = np.mean(his[:, 0])
+        macd = calc_ema(his, 12) - calc_ema(his, 26)
+
+        technical_indicators.append(np.array([macd]))
+        # technical_indicators.append(np.array([sma,macd,]))
+
+    technical_indicators = np.array(technical_indicators)
+    tech_ind_scaler = preprocessing.MinMaxScaler()
+    technical_indicators_normalised = tech_ind_scaler.fit_transform(technical_indicators)
+    assert ohlcv_histories_normalised.shape[0] == next_day_open_values_normalised.shape[0] == technical_indicators_normalised.shape[0]
+
+    test_split = 0.7
+    n = int(ohlcv_histories_normalised.shape[0] * test_split)
+
+    ohlcv_train = ohlcv_histories_normalised[:n]
+    tech_ind_train = technical_indicators_normalised[:n]
+    y_train = next_day_open_values_normalised[:n]
+    ohlcv_test = ohlcv_histories_normalised[n:]
+    tech_ind_test = technical_indicators_normalised[n:]
+    y_test = next_day_open_values_normalised[n:]
+    unscaled_y_test= next_day_open_values[n:]
+
+    lstm_input = Input(shape=(history_points,1), name='lstm_input')
+    dense_input = Input(shape=(technical_indicators.shape[1],), name='tech_input')
+    #######
+    model = lstm_model_tech(lstm_input,dense_input,ohlcv_train,tech_ind_train,y_train)
+    #######
+    y_test_predicted = model.predict([ohlcv_test, tech_ind_test])
+    y_predicted = model.predict([ohlcv_histories_normalised, technical_indicators_normalised])
+    assert unscaled_y_test.shape == y_test_predicted.shape
+
+    #print(y_test_predicted)
+    assert unscaled_y_test.shape == y_test_predicted.shape
+
+    mse = mean_squared_error(y_test,y_test_predicted)
+    #print("R2",r2_score(y_test,y_test_predicted))
+    mae = mean_absolute_error(y_test,y_test_predicted)
+
+
+    y_test_predicted = y_normaliser.inverse_transform(y_test_predicted)
+    #y_predicted = y_normaliser.inverse_transform(y_predicted)
+
+    real_mse = np.mean(np.square(unscaled_y_test - y_test_predicted))
+    scaled_mse = real_mse / (np.max(unscaled_y_test) - np.min(unscaled_y_test)) * 100
+
+    plt.plot(range(n,len(data)-51), unscaled_y_test[:-1], label='real')
+    plt.plot(range(n,len(data)-51),y_test_predicted[:-1], label='predicted')
+    plt.legend(['Real', 'Predicted'])
+
+    plt.show()
+    st.pyplot()
+    return mse,mae,scaled_mse
+
 
 
 def get_historical(ticker):
@@ -338,8 +469,12 @@ def rolling_sharpe_plot(ticker, start, end):
     sp500_ret = sp500['Close'].pct_change()[1:]
 
     days2 = st.slider('Business Days to roll', 5, 130, 50)
-    rs_sp500 = sp500_ret.rolling(days2).apply(rolling_sharpe)[:]
     rs = ret.rolling(days2).apply(rolling_sharpe)
+    if len(rs.index) == len(sp500_ret):
+        rs_sp500 = sp500_ret.rolling(days2).apply(rolling_sharpe)[:]
+    else:
+        rs_sp500 = sp500_ret.rolling(days2).apply(rolling_sharpe)[:-1]
+
     fig, ax = plt.subplots(figsize=(10,4))
     ax.plot(rs.index, rs.values, 'b-', label = 'Geometric Rolling Sharpe %s'%ticker)
     ax.plot(rs.index, rs_sp500, 'r-', label = 'Geometric Rolling Sharpe S&P500 (TR)')
@@ -560,19 +695,19 @@ def recommending(df, global_polarity, today_stock, mean=1.5):
 
 
 
-st.cache(suppress_st_warning=True)
+@st.cache(suppress_st_warning=True)
 def def_model_lstm_GRU(X_train,y_train,X_test, ytest):
     model = Sequential()
     model.add(LSTM(10, return_sequences=True,input_shape=(50, 1)))
     model.add(GRU(10))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam',)
-    model.fit(X_train, y_train, validation_data=(X_test, ytest), epochs=15, batch_size=64, verbose=1)
+    model.fit(X_train, y_train, validation_data=(X_test, ytest), epochs=30, batch_size=64, verbose=1)
     train_predict = model.predict(X_train)
     test_predict = model.predict(X_test)
     return train_predict,test_predict,model
 
-@st.cache(suppress_st_warning=True)
+
 def lstm():
     from sklearn.preprocessing import MinMaxScaler
     df = loadData(ticker,start,end)
@@ -597,7 +732,7 @@ def lstm():
     X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
     import time
 
-    st.cache(suppress_st_warning=True)
+    @st.cache(suppress_st_warning=True)
     def def_model_lstm_stacked(X_train,y_train,X_test, ytest):
         model = Sequential()
         model.add(LSTM(10, return_sequences=True,input_shape=(50, 1)))
@@ -605,7 +740,7 @@ def lstm():
         model.add(Dense(1))
         model.compile(loss='mean_squared_error', optimizer='adam')
 
-        model.fit(X_train,y_train,validation_data=(X_test,ytest),epochs=10,batch_size=40,verbose=1)
+        model.fit(X_train,y_train,validation_data=(X_test,ytest),epochs=30,batch_size=30,verbose=1)
         train_predict = model.predict(X_train)
         test_predict = model.predict(X_test)
         return train_predict,test_predict,model
@@ -867,9 +1002,11 @@ if check_dates() and pivot_date == True:
 
         series_info.name = 'Stock'
 
+
         principal_graphs_checkbox = st.sidebar.checkbox('Stock Visualisation', value = True)
         if principal_graphs_checkbox:
             st.title('Stock Visualisation')
+            st.dataframe(series_info)
             plotData1(ticker, start, end)
 
         trailing_checkbox = st.sidebar.checkbox('Historical Prices and Volume')
